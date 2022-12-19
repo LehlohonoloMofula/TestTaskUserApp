bce48ac7bd77006ecbad2bdef4e47515fd25acde
Lehlohonolo Mofula <lehlohonoloharison@gmail.com>
2022-12-19 22:25:53 +0200

    teyi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
